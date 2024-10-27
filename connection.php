a1a716c08c666702aed2fc11617934dab2ce380f
dayangnurnazihah <dayangziha@gmail.com>
2024-10-27 13:40:28 +0800

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php
 
 $servername = 'localhost';
-$username = 'project';
+$username = 'u686273261_project';
 $password = 'G03_01ipss';
-$db = 'ipss';
+$db = 'u686273261_ipss';
 
 $conn = new mysqli($servername, $username, $password, $db);
 
