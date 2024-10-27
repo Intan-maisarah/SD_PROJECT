e2316fcfee71cafe35c441db5de4fe03b4af6a02
dayangnurnazihah <dayangziha@gmail.com>
2024-10-27 13:32:52 +0800

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,12 +1,12 @@
 <?php
-$servername = "localhost";
-$username = "project";
-$password = "project1";
-$db = "ipss";
+
+$servername = 'localhost';
+$username = 'project';
+$password = 'G03_01ipss';
+$db = 'ipss';
 
 $conn = new mysqli($servername, $username, $password, $db);
 
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    exit('Connection failed: '.$conn->connect_error);
 }
-?>
